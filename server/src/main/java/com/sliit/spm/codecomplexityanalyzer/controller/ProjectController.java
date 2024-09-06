5337e6761a86795c7a1962cab7d71ab5fc3abdc9
UdeeshaRukshan <IT21807480@my.sliit.lk>
2024-09-06 21:53:02 +0530

    added new models and services

---
+++ server/src/main/java/com/sliit/spm/codecomplexityanalyzer/controller/ProjectController.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.sliit.spm.codecomplexityanalyzer.controller;
+import com.sliit.spm.codecomplexityanalyzer.service.serviceimpl.ProjectService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author it16166752
+ *
+ */
+@CrossOrigin(origins = "*")
+@RestController
+public class ProjectController {
+	@Autowired
+	ProjectService projectService;
+
+	@PostMapping("/projects")
+	public ResponseEntity<?> saveProject(@RequestBody Project project) {
+		return new ResponseEntity<>(projectService.save(project), HttpStatus.OK);
+	}
+
+	@GetMapping("/projects/{key}")
+	public ResponseEntity<?> getProject(@PathVariable("key") String key) {
+		return new ResponseEntity<>(projectService.getByKey(key), HttpStatus.OK);
+	}
+
+	@GetMapping("/projects/{key}/history")
+	public ResponseEntity<?> getProjectHistory(@PathVariable("key") String key) {
+		return new ResponseEntity<>(projectService.getHistoryByKey(key), HttpStatus.OK);
+	}
+
+	@GetMapping("/projects")
+	public ResponseEntity<?> getProjects() {
+		return new ResponseEntity<>(projectService.getAll(), HttpStatus.OK);
+	}
+}
